18ba655d8f595a02699bf9f4eb3ad02ecb105515
Gabriel Chiavegatto Gomes <chiavegattogomes@gmail.com>
2022-02-03 03:31:00 -0300

    quantidade de cartas aplicadas de acordo o usuário

--- jogo.js
+++ jogo.js
@@ -1,18 +1,34 @@
- let numeroDeCartas = null;
+let numeroDeCartas = null;
+const cartas = [];
 
- function quantidadeDeCartas(){
+function quantidadeDeCartas() {
 
-     const qntCartas = prompt("Bem Vindo ao Parrot! \nPor favor digite com quantas cartas você deseja jogar. \nAtenção!! Deve ser um número PAR entre 4 e 14.");
-     if( qntCartas >= 4 && qntCartas <= 14 && qntCartas%2 == 0) {
-         numeroDeCartas = qntCartas;
-     } else { 
-         quantidadeDeCartas();
+    const qntCartas = prompt("Bem Vindo ao Parrot! \nPor favor digite com quantas cartas você deseja jogar. \nAtenção!! Deve ser um número PAR entre 4 e 14.");
+    if (qntCartas >= 4 && qntCartas <= 14 && qntCartas % 2 == 0) {
+        numeroDeCartas = qntCartas;
+    } else {
+        quantidadeDeCartas();
     }
- }
- quantidadeDeCartas()
+}
 
 
+function aplicarQuantidadeDeCartas() {
+    const main = document.querySelector("main");
+    
+    for (let i = 0; i < numeroDeCartas; i++) {
+        cartas[i]=` <div class="carta" onclick="contadorJogadas()"><img src="bobrossparrot.gif" alt="gif"></div>`;
+        main.innerHTML += `${cartas[i]}`;
+    }
+
+}
+
+// contador de jogadas
 let jogadas = 0;
-function contadorJogadas(){
+function contadorJogadas() {
     jogadas += 1;
 }
+
+
+//Chamando as funções: 
+quantidadeDeCartas();
+aplicarQuantidadeDeCartas();
