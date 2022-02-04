f646b7b2bfe6ec9103fd1be42c6556a4dbf350ba
Gabriel Chiavegatto Gomes <chiavegattogomes@gmail.com>
2022-02-04 14:04:26 -0300

    tentativas de aplcar aleatóriedade, inicio das cartas viradas

--- jogo.js
+++ jogo.js
@@ -2,6 +2,12 @@ let numeroDeCartas = null;
 const cartas = [];
 const gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
 
+let paresEncontrados = 0;
+
+
+
+
+
 function quantidadeDeCartas() {
 
     const qntCartas = prompt("Bem Vindo ao Parrot! \nPor favor digite com quantas cartas você deseja jogar. \nAtenção!! Deve ser um número PAR entre 4 e 14.");
@@ -16,6 +22,7 @@ function quantidadeDeCartas() {
 function aplicarQuantidadeDeCartas() {
     const main = document.querySelector("main");
     const pares = (numeroDeCartas / 2);
+    
     let gifNaCarta;
     for (let i = 0; i < pares; i++) {
         gifNaCarta = gifs[i];
@@ -28,24 +35,41 @@ function aplicarQuantidadeDeCartas() {
     for (let j = 0; j < pares; j++) {
         cartas.push(cartas[j]);
     }
-    main.innerHTML = cartas;
+    
+    // Fazer o random aqui antes do inner HTML
+
+    for (let k = 0; k < cartas.length; k++) {
+        main.innerHTML += (cartas[k]);
+    }
 }
+
+// function myFunction() {
+//     cartas.sort(function ( ) { return 0.5 - Math.random() });
+    
+// }
 
 
 function virarCarta(clique) {
     clique.classList.add("clicked");
-    const trocarImg = document.querySelector(".clicked .verso");
+    const trocarImg = clique.querySelector(".verso");
     if (trocarImg !== null) {
         trocarImg.classList.add("escondido");
-        const colocaGif = document.querySelector(".clicked .frente");
+        const colocaGif = clique.querySelector(".frente");
         colocaGif.classList.remove("escondido");
     }
-
-
-
-
     contadorJogadas();
+    // verificarCartasViradas();
 }
+
+// function verificarCartasViradas(){
+//     const procuraPares = document.querySelectorAll(".cliked")
+//     if(a carta for igualzinha){
+//         paresEncontrados += 1;
+        
+
+//     }
+// }
+
 // contador de jogadas
 let jogadas = 0;
 function contadorJogadas() {
@@ -54,5 +78,7 @@ function contadorJogadas() {
 
 
 //Chamando as funções: 
+
 quantidadeDeCartas();
 aplicarQuantidadeDeCartas();
+// myFunction();
