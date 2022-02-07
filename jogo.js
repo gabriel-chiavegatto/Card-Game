748e450b7309cfb1452a6bee128c2ab76519ad5b
Gabriel Chiavegatto Gomes <chiavegattogomes@gmail.com>
2022-02-07 16:49:48 -0300

    entrega

--- jogo.js
+++ jogo.js
@@ -8,11 +8,7 @@ let carta2 = null;
 let teste = true;
 let tempo = 0;
 const relogio = document.querySelector("footer");
-
 let jogadas = 0;
-
-
-
 
 
 function quantidadeDeCartas() {
@@ -50,38 +46,36 @@ function aplicarQuantidadeDeCartas() {
     for (let k = 0; k < cartas.length; k++) {
         main.innerHTML += (cartas[k]);
     }
-} 
+}
+
 
 function comparador() {
     return Math.random() - 0.5;
 }
 
 
-
-
 function virarCarta(clique) {
-    const temCartaVirada = document.querySelector(".clicked");
+    if (carta2 == null) {
+        const temCartaVirada = document.querySelector(".clicked");
 
-    if (temCartaVirada == null) {
-        clique.classList.add("clicked");
-        clique.classList.add("rotacao");
-        const clicada = clique.querySelector(".frente");
-        carta1 = clicada;
+        if (temCartaVirada == null) {
+            clique.classList.add("clicked");
+            clique.classList.add("rotacao");
+            const clicada = clique.querySelector(".frente");
+            carta1 = clicada;
+        }
+        else {
+            clique.classList.add("clicked2");
+            clique.classList.add("rotacao");
+            const clicada2 = clique.querySelector(".frente");
+            carta2 = clicada2;
+        }
+        setTimeout(colocaGif, 300);
+        contadorJogadas();
+
     }
-    else {
-        clique.classList.add("clicked2");
-        clique.classList.add("rotacao");
-        const clicada2 = clique.querySelector(".frente");
-        carta2 = clicada2;
-    }
-    setTimeout(colocaGif, 1000);
-    contadorJogadas();
-    
-
-
-
-
 }
+
 
 function colocaGif() {
     const trocarImg1 = document.querySelector(".clicked .verso");
@@ -96,13 +90,15 @@ function colocaGif() {
         const gifIn = document.querySelector(".clicked2 .frente");
         gifIn.classList.remove("escondido");
         setTimeout(verificarPar, 1500);
+
     }
-    
+
 }
+
 
 function verificarPar() {
     if (carta1.isEqualNode(carta2)) {
-        console.log("deu");
+        // console.log("bingo");
         let achouOPar1 = document.querySelector(".clicked");
         achouOPar1.classList.add("encontrada");
         achouOPar1.classList.remove("clicked");
@@ -110,9 +106,10 @@ function verificarPar() {
         achouOPar2.classList.add("encontrada");
         achouOPar2.classList.remove("clicked2");
         encontradas += 2;
+       
 
     } else {
-        console.log("fold");
+        // console.log("fold");
         //carta1
         let errou1 = document.querySelector(".clicked .frente");
         errou1.classList.add("escondido");
@@ -135,6 +132,7 @@ function verificarPar() {
     jogoFinalizado();
 }
 
+
 // function aumentaSegundos() {
 //         relogio.innerHTML = parseInt(relogio.innerHTML) + 1;
 // }
@@ -144,35 +142,33 @@ function contadorJogadas() {
     jogadas += 1;
 }
 
-function jogoFinalizado(){
-    if (encontradas == numeroDeCartas){
+
+function jogoFinalizado() {
+    if (encontradas == numeroDeCartas) {
         alert(`Parabéns! Você ganhou em ${jogadas} jogadas`);
         jogarMaisUmaVez();
     }
 }
-    
-    
-    function jogarMaisUmaVez (){
-        jogarDeNovo = prompt(`Você deseja jogar de novo? (Responda com S ou N)`);
-        if(jogarDeNovo == "S" || jogarDeNovo == "s") {
-            let reinicia = true;
-            document.location.reload(reinicia);
-            return;
-        } if(jogarDeNovo == "N" || jogarDeNovo == "n"){
-            return;
-        } else { jogarMaisUmaVez();  }
-        
-    }
-
-    
-
-  
 
 
-    //Chamando as funções: 
+function jogarMaisUmaVez() {
+    jogarDeNovo = prompt(`Você deseja jogar de novo? (Responda com S ou N)`);
+    if (jogarDeNovo == "S" || jogarDeNovo == "s") {
+        let reinicia = true;
+        document.location.reload(reinicia);
+        return;
+    } if (jogarDeNovo == "N" || jogarDeNovo == "n") {
+        return;
+    } else { jogarMaisUmaVez(); }
 
-    quantidadeDeCartas();
-    aplicarQuantidadeDeCartas();
-    
+}
+
+
+
+//Chamando as funções: 
+
+quantidadeDeCartas();
+aplicarQuantidadeDeCartas();
+
 
 
